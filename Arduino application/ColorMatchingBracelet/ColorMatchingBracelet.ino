db7fb49ba2c40d0afedd25154a862a7958c8adc1
xrgman <xrgman@gmail.com>
2022-10-10 13:56:33 +0200

    Esp32 bluetooth code

--- Arduino application/ColorMatchingBracelet/ColorMatchingBracelet.ino
+++ Arduino application/ColorMatchingBracelet/ColorMatchingBracelet.ino
@@ -1,6 +1,15 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
-// #include "FastLED.h"
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define SERVICE_UUID           "1cf4fab1-d642-4153-a6f2-bf40db8d6f73" // UART service UUID
+#define CHARACTERISTIC_UUID_RX "1cf4fab1-d642-4153-a6f2-bf40db8d6f73"
+#define CHARACTERISTIC_UUID_TX "1cf4fab1-d642-4153-a6f2-bf40db8d6f73"
+#define BLUETOOTH_NAME "Color Matching Bracelet"
 
 #define LED_PIN 13
 #define NUM_PIXELS 5 //Nr of leds in bracelet
@@ -8,21 +17,100 @@
 //Creating led strip object:
 Adafruit_NeoPixel bracelet(NUM_PIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+//Bluetooth vars:
+BLEServer *pServer = NULL;
+BLECharacteristic * pTxCharacteristic;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+
+/**
+  Callback used when device is connected or disconnected:
+**/
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      Serial.println("Device connected");
+
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      Serial.println("Device disconnected");
+      
+      deviceConnected = false;
+    }
+};
+
+/**
+  Callback used upon receiving data:
+**/
+class MyCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string rxValue = pCharacteristic->getValue();
+
+      if (rxValue.length() > 0) {
+        Serial.println("*********");
+        Serial.print("Received Value: ");
+
+        for (int i = 0; i < rxValue.length(); i++){
+            Serial.print(rxValue[i]);          
+        }
+          
+        Serial.println();
+        Serial.println("*********");
+      }
+    }
+};
+
+
 void setup() {
+  Serial.begin(115200);
+
+  //Starting ledstrip:
   bracelet.begin();
-  Serial.begin(9600);
+
+  //Starting bluetooth:
+  BLEDevice::init(BLUETOOTH_NAME);
+
+  //Create the bluetooth server and setting callback:
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  //Create bluetooth service and setting characteristics:
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
+  
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
+
+  pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+  //Starting bluetooth service:
+  pService->start();
+
+  //Start advertising:
+  pServer->startAdvertising();
+
+  Serial.println("Waiting for client connection");
 }
 
 void loop() {
-  bracelet.clear(); 
 
-  for(int i=0; i<NUM_PIXELS; i++) {
-      bracelet.setPixelColor(i, bracelet.Color(10, 150, 0));
-      delay(1);
-      bracelet.show();
-      delay(500);
-      //Serial.println("test");
-  }
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("Waiting for client connection");
+        oldDeviceConnected = deviceConnected;
+    }
 
-  delay(1000);
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+		// do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }  
+
+  
 }
